f62adb49db17e70e74cdd58994bc1227e7cad993
PRADIP-1596 <9187pradipyadav@gmail.com>
2021-11-30 12:09:22 +0530

    [Ashish] Added:Intialize The Projects

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import Design from './DesignFlow/Design';
+import  Sign from './Sign-Up/Sign';
+
+function App() {
+  return (
+  <>
+
+    <Design/>
+  
+   {/* <Sign/> */}
+
+  </>
+  );
+}
+
+export default App;
